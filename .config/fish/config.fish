ae0d78e52ed25f59a9077c56f86a20567958fdeb
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2025-04-08 17:07:58 +0330

    use rofi instead of dmenu!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,3 @@ if status is-interactive
 end
 
 thefuck --alias | source
-if not set -q SSH_AUTH_SOCK
-    eval (ssh-agent -c | sed 's/^setenv/set -gx/')
-    ssh-add ~/.ssh/id_ed25519 2>/dev/null
-end
