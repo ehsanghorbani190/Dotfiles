0f62649f83c0cb62b94abd5a77ed3f1d888dfdd2
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2026-08-12 15:29:03 +0330

    [feat] remove fuck, add maven rajman ssh key to keychain

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,8 @@
 if status is-interactive
-  eval (keychain --eval -Q -q --noask ~/.ssh/id_ed25519)
+  eval (keychain --eval -Q -q --noask ~/.ssh/id_ed25519 ~/.ssh/maven_rajman)
   
   set -gx PATH $HOME/.jenv/bin $PATH
   source (jenv init --no-rehash - | psub)
 end
-
-thefuck --alias | source
 
 fish_vi_key_bindings
