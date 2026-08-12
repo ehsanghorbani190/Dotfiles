de52f7addab41ba67ca3daa9b894975cf70aa5bb
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2026-08-12 23:15:36 +0330

    [feat] add ssh keys to keychain on startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-  eval (keychain --eval -Q -q --noask ~/.ssh/id_ed25519 ~/.ssh/maven_rajman)
+  eval (keychain --eval -q ~/.ssh/github ~/.ssh/gitlab_rajman)
   
   set -gx PATH $HOME/.jenv/bin $PATH
   source (jenv init --no-rehash - | psub)
