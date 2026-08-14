4e77d881345321691284354bb92d8c1417d7330c
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2026-08-14 12:20:14 +0330

    [feat] add man page and forture to fish config, use --no-erase to prevent autopair from breaking

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,19 @@
-if status is-interactive
-  eval (keychain --eval -q ~/.ssh/github ~/.ssh/gitlab_rajman)
-  
-  set -gx PATH $HOME/.jenv/bin $PATH
-  source (jenv init --no-rehash - | psub)
+fish_vi_key_bindings --no-erase
+
+function fish_greeting
+    fortune | cowsay
 end
 
-fish_vi_key_bindings
+if status is-interactive
+    eval (keychain --eval -q ~/.ssh/github ~/.ssh/gitlab_rajman)
+
+    fish_add_path $HOME/.jenv/bin
+    source (jenv init --no-rehash - | psub)
+end
+
+
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+alias mirror='update_mirror'
+alias update='update_mirror && sudo pacman -Syu'
